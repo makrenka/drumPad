dd0f9efc836776c1788166f963573e1ae06c8752
makrenka <smokrenko79@gmail.com>
2022-10-17 19:33:38 +0300

    add animatedPad and play

--- drumpad.js
+++ drumpad.js
@@ -1,20 +1,59 @@
-import { SOUNDS, KEY_MAP } from "./constants";
+import { SOUNDS, KEY_MAP } from "./constants.js";
 
 const pads = document.querySelectorAll('.pad');
+const visual = document.querySelector('.visual');
 
 
+const playSound = (sounds, index) => {
+    const sound = sounds[index];
+    const audio = new Audio(sound);
+    audio.load();
+    audio.play();
+};
 
-const loadSounds = (sounds) => {
-    sounds.forEach((sound) => {
-        const audio = new Audio(sound);
-        audio.load()
+const animatePad = (index) => {    
+    pads[index].style.animation = 'dance 0.1s';
+    pads[index].addEventListener('animationend', () => {
+        pads[index].style.animation = 'none';
     })
+};
+
+const createBubble = (index) => {
+    const bubble = document.createElement('div');
+    visual.appendChild(bubble);
+
 }
 
-loadSounds(SOUNDS);
+const play = (index) => {
+    playSound(SOUNDS, index);
+    animatePad(index);
+    createBubble(index);
+};
 
 const keyPressHandler = (evt) => {
-    console.log(evt.key);
+    switch (evt.key) {
+        case KEY_MAP.kick:
+            play(0);
+            break;
+        case KEY_MAP.cymbal:
+            play(1);
+            break;
+        case KEY_MAP.snare:
+            play(2);
+            break;
+        case KEY_MAP.openhat:
+            play(3);
+            break;
+        case KEY_MAP.longCrash:
+            play(4);
+            break;
+        case KEY_MAP.hihat:
+            play(5);
+            break;
+    
+        default:
+            break;
+    }
 }
 
 document.addEventListener('keypress', keyPressHandler);
