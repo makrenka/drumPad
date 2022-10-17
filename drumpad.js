f4fc001621e628e6680ba596cea09d44d1f51f86
makrenka <smokrenko79@gmail.com>
2022-10-17 13:27:29 +0300

    add index.html and css

---
+++ drumpad.js
@@ -0,0 +1,20 @@
+const pads = document.querySelectorAll('.pad');
+
+const SOUNDS = [
+    "sounds/kick.wav",
+    "sounds/cymbal.wav",
+    "sounds/snare.wav",
+    "sounds/openhat.wav",
+    "sounds/long-crash.wav",
+    "sounds/hihat.wav",
+];
+
+const loadSounds = () => {
+
+}
+
+const keyPressHandler = (evt) => {
+    console.log(evt.key);
+}
+
+document.addEventListener('keypress', );
